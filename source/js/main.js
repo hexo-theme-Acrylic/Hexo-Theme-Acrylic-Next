550754adaa2889cbbfc4ed12e44c7d85552e09f1
rootlex <59170489+rootlexblog@users.noreply.github.com>
2023-03-03 18:21:47 +0800

    ✨ feat: 增加对于静态说说的支持

--- source/js/main.js
+++ source/js/main.js
@@ -68,82 +68,81 @@ const sidebarFn = () => {
 
 const showTodayCard = () => {
     const el = document.getElementById('todayCard')
-    if(el){
+    if (el) {
         document.getElementsByClassName('topGroup')[0].addEventListener('mouseleave', () => {
-            if(el.classList.contains('hide')){
+            if (el.classList.contains('hide')) {
                 el.classList.remove('hide')
             }
         })
-    }  
+    }
 }
 
 const setTimeState = () => {
     const el = document.getElementById('author-info__sayhi')
-    if(el){
+    if (el) {
         const timeNow = new Date();
         const hours = timeNow.getHours();
         let text = '';
         if (hours >= 0 && hours <= 5) {
-        text = '晚安';
+            text = '晚安';
         } else if (hours > 5 && hours <= 10) {
-        text = '早上好';
+            text = '早上好';
         } else if (hours > 10 && hours <= 14) {
-        text = '中午好';
+            text = '中午好';
         } else if (hours > 14 && hours <= 18) {
-        text = '下午好';
+            text = '下午好';
         } else if (hours > 18 && hours <= 24) {
-        text = '晚上好';
+            text = '晚上好';
         }
-        el.innerText= text + ' !  我是'
+        el.innerText = text + ' !  我是'
     }
 };
 
 const percent = () => {
     let a = document.documentElement.scrollTop || window.pageYOffset,
-      b = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight, document.body.clientHeight, document.documentElement.clientHeight) - document.documentElement.clientHeight, // 整个网页高度
-      result = Math.round(a / b * 100),
-      btn = document.querySelector("#percent");
+        b = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight, document.body.clientHeight, document.documentElement.clientHeight) - document.documentElement.clientHeight, // 整个网页高度
+        result = Math.round(a / b * 100),
+        btn = document.querySelector("#percent");
     const visibleBottom = window.scrollY + document.documentElement.clientHeight;
     const eventlistner = document.getElementById('post-tools') || document.getElementById('footer');
     const centerY = eventlistner.offsetTop + (eventlistner.offsetHeight / 2);
     if ((centerY < visibleBottom) || (result > 90)) {
-      document.querySelector("#nav-totop").classList.add("long");
-      btn.innerHTML = "返回顶部";
+        document.querySelector("#nav-totop").classList.add("long");
+        btn.innerHTML = "返回顶部";
     } else {
-      document.querySelector("#nav-totop").classList.remove("long");
-      if (result >= 0) {
-        btn.innerHTML = result;
-      }
+        document.querySelector("#nav-totop").classList.remove("long");
+        if (result >= 0) {
+            btn.innerHTML = result;
+        }
     }
 }
 
 class toc {
     static init() {
         const el = document.querySelectorAll('.toc a')
-        if(!el){
+        if (!el) {
             return
         }
         el.forEach((e) => {
             e.addEventListener('click', (event) => {
                 event.preventDefault()
-                // replace是替换空格，避免跳转报错
                 this.getAnchor(event.target.innerText.replaceAll(' ', '-'))
             })
         })
         this.active(el)
     }
-    static scrollToAnchor(x, y){
+    static scrollToAnchor(x, y) {
         scrollTo({
             top: y,
             left: x,
             behavior: 'smooth'
         })
     }
-    static getAnchor(id){
+    static getAnchor(id) {
         const el = document.getElementById(id)
         this.scrollToAnchor(el.getBoundingClientRect().left, el.getBoundingClientRect().top + (window.scrollY ? window.scrollY : 0) - 60)
     }
-    static active(toc){
+    static active(toc) {
         const $article = document.getElementById('article-container')
         const list = $article.querySelectorAll('h1,h2,h3,h4,h5,h6')
         let detectItem = ''
@@ -156,7 +155,7 @@ class toc {
 
             list.forEach(function (ele, index) {
                 if (top > utils.getEleTop(ele) - 80) {
-                currentIndex = index
+                    currentIndex = index
                 }
             })
 
@@ -165,7 +164,7 @@ class toc {
             document.querySelectorAll('.active').forEach((i) => {
                 i.classList.remove('active')
             })
-            if (toc[detectItem]){
+            if (toc[detectItem]) {
                 let parent = toc[detectItem].parentNode
                 toc[detectItem].classList.add('active')
                 for (; !parent.matches('.toc'); parent = parent.parentNode) {
@@ -173,11 +172,11 @@ class toc {
                 }
             }
         }
-        window.tocScrollFn = utils.throttle(function() {
+        window.tocScrollFn = utils.throttle(function () {
             const currentTop = window.scrollY || document.documentElement.scrollTop
             findHeadPosition(currentTop)
-          }, 100)
-      
+        }, 100)
+
         window.addEventListener('scroll', tocScrollFn)
     }
 }
@@ -201,63 +200,88 @@ class acrylic {
     }
     static toTop() {
         window.scrollTo({
-            top:0,
-            behavior:"smooth"
+            top: 0,
+            behavior: "smooth"
         })
     }
-    static showConsole(){
+    static showConsole() {
         const el = document.getElementById('console')
-        if(!el.classList.contains('show')){
+        if (!el.classList.contains('show')) {
             el.classList.add('show')
         }
     }
-    static hideConsole(){
+    static hideConsole() {
         const el = document.getElementById('console')
-        if(el.classList.contains('show')){
+        if (el.classList.contains('show')) {
             el.classList.remove('show')
         }
     }
-    static async copyPageUrl(){
+    static async copyPageUrl() {
         try {
             await navigator.clipboard.writeText(window.location.href)
             utils.snackbarShow('已将页面链接复制至剪贴板', false, 2000)
-          } catch (err) {
+        } catch (err) {
             utils.snackbarShow('无法将页面链接复制至剪贴板 : ' + err, false, 2000)
-          }
+        }
     }
-    static lightbox(){
-        window.ViewImage && ViewImage.init('#article-container img');
+    static lightbox() {
+        window.ViewImage && ViewImage.init('#article-container img, #bber .bber-content-img img');
     }
-    static initTheme(){
-       const nowMode = localStorage.getItem('theme')
-       if(nowMode === 'dark')document.documentElement.setAttribute('data-theme', 'dark')
-       if(nowMode === 'light')document.documentElement.setAttribute('data-theme', 'light')
+    static initTheme() {
+        const nowMode = localStorage.getItem('theme')
+        if (nowMode === 'dark') document.documentElement.setAttribute('data-theme', 'dark')
+        if (nowMode === 'light') document.documentElement.setAttribute('data-theme', 'light')
+    }
+    static reflashEssayWaterFall() {
+        if (document.getElementById('waterfall')) {
+            setTimeout(function(){
+                waterfall('#waterfall');
+                document.getElementById("waterfall").classList.add('show'); 
+            },500);
+        }
     }
 }
 
 const onlyHome = () => {
-    if(GOBALPAGE === 'home'){
+    if (GOBALPAGE === 'home') {
         showTodayCard()
         if (typeof randomLinksList === 'function') {
             randomLinksList();
+        }
+        if (document.querySelector('#bber-talk')) {
+            var swiper = new Swiper('.swiper-container', {
+              direction: 'vertical',
+              loop: true,
+              autoplay: {
+              delay: 3000,
+              pauseOnMouseEnter: true
+            },
+            });
         }
     }
 }
 
 const onlyPost = () => {
-    if(GOBALPAGE === 'post'){
+    if (GOBALPAGE === 'post') {
         toc.init()
     }
 }
 
 const onlyPostandPage = () => {
-    if(GOBALPAGE === 'post' || GOBALPAGE === 'page'){
+    if (GOBALPAGE === 'post' || GOBALPAGE === 'page') {
         acrylic.lightbox()
         if (typeof initComment === 'function') {
             initComment();
         }
     }
+    acrylic.reflashEssayWaterFall()
 }
+
+window.addEventListener('resize', utils.throttle(function () {
+    if (document.getElementById('waterfall')) {
+        acrylic.reflashEssayWaterFall();
+    }
+}), 500);
 
 window.addEventListener('DOMContentLoaded', () => {
     sidebarFn()
@@ -268,6 +292,7 @@ window.addEventListener('DOMContentLoaded', () => {
     setTimeState()
     acrylic.initTheme()
 })
+
 document.addEventListener('pjax:complete', function () {
     sidebarFn()
     scrollFn()
