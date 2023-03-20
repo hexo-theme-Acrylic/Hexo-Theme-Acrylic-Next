ac7f5a71dc1e5782894f2161fcc7515d3082f983
rootlex <59170489+rootlexblog@users.noreply.github.com>
2023-03-20 12:05:03 +0800

    🎈 perf: 优化懒加载实现方法

--- scripts/filter/lazyload.js
+++ scripts/filter/lazyload.js
@@ -4,6 +4,8 @@ hexo.extend.filter.register('after_render:html', function (data) {
     const config = hexo.theme.config.lazyload
     if (!config.enable) return
     const bg = config.placeholder ? urlFor(config.placeholder) : 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
-    return data.replace(/(<img.*? src=)/ig, `$1 "${bg}" data-lazy-src=`)
+    return data.replace(
+        /(<img(?!.*?class[\t]*=[\t]*['"].*?nolazyload.*?['"]).*? src=)/gi,
+        `$1 "${bg}" data-lazy-src=`
+    )
 })
-  
