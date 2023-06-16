234a5e090398bc2e5fcb8d8a1140ed2d8b8982a2
Adil <adil_zhang@163.com>
2023-06-16 10:01:31 +0800

    🎈 perf: node版本过低时警告

--- scripts/event/init.js
+++ scripts/event/init.js
@@ -1,6 +1,8 @@
 hexo.extend.filter.register('before_generate', () => {
     // Get first two digits of the Hexo version number
     const hexoVer = hexo.version.replace(/(^.*\..*)\..*/, '$1')
+    const nodeVer = process.version.replace(/^v/, '');
+    const [majorVer] = nodeVer.split('.');
     const logger = hexo.log, config = hexo.config
   
     if (hexoVer < 5.3) {
@@ -12,4 +14,9 @@ hexo.extend.filter.register('before_generate', () => {
       logger.error('主题尚未支持 prismjs 请使用 highlightjs ！')
       process.exit(-1)
     }
-  })
+    if (Number(majorVer) < 14) {
+      logger.error('请将 Node.js 升级到 v14.0.0 或更高的版本！');
+      process.exit(-1);
+  }
+  }
+  )
